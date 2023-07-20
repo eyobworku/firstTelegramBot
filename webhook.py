9c0701f83a0fd4b6b177af9c62876a673a54d34c
eyob worku <eyobworku62@gmail.com>
2023-07-20 11:09:10 +0300

    database taste

--- webhook.py
+++ webhook.py
@@ -1,27 +1,42 @@
 from flask import Flask, request
-from functions import *
+# from functions import *
+from flask_pymongo import PyMongo
+# from bson.objectid import ObjectId
+import os
 
 # Define your bot token
 # BOT_TOKEN = '6003301685:AAFSmqvk4IWe7mn9xt2HsLagLMjV1CIzpJA'
 
 app = Flask(__name__)
+app.config['MONGO_URI']='mongodb+srv://eyob:12345ewp@mydatabase.g895nzq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+mongo = PyMongo()
+mongo.init_app(app)
 
-@app.route('/', methods=['POST'])
-def webhook():
-    # Get the JSON data from the request
-    update = request.get_json()
+# @app.route('/', methods=['POST'])
+# def webhook():
+#     # Get the JSON data from the request
+#     update = request.get_json()
 
-    # Initialize two variables used to respond to Telegram
-    methods = []
-    post_fields = [{}]
+#     # Initialize two variables used to respond to Telegram
+#     methods = []
+#     post_fields = [{}]
 
-    # Do the thing
-    echo_input(update)
+#     # Do the thing
+#     echo_input(update)
 
-    # Send it all to Telegram's servers using HTTP POST
-    send_response(update)
+#     # Send it all to Telegram's servers using HTTP POST
+#     send_response(update)
 
-    return 'OK'
+#     return 'OK'
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/', methods=['GET'])
+def index():
+    db = mongo.db
+    bool = db.user.insert_one({'name':'test'})
+    if(bool.acknowledged):
+        return '<h1>Added user</h1>'
+    else:
+        return '<h1>Not added user</h1>'
+
+# if __name__ == '__main__':
+#     app.run()
