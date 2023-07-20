750b52061a918d550f73e91e9d96a0c4a3c5de80
eyob worku <eyobworku62@gmail.com>
2023-07-20 18:47:52 +0300

    add methods

--- functions.py
+++ functions.py
@@ -28,97 +28,102 @@ def send_response(update):
             # Process the response if needed
 
 def route_requests(update):
-    if hasattr(update.message, 'entities') and update.message.entities and update.message.entities[0].type == 'bot_command':
-        update.parameters = []
-        update.command = [update.message.text[0:update.message.entities[0].length]]
-        update.parameters.append(update.message.text[update.message.entities[0].length+1:])
+    if 'entities' in update['message'] and update['message']['entities'] and update['message']['entities'][0]['type'] == 'bot_command':
+        update['parameters'] = []
+        update['command'] = [update['message']['text'][0:update['message']['entities'][0]['length']]]
+        update['parameters'].append(update['message']['text'][update['message']['entities'][0]['length']+1:])
 
-        if update.command[0] == '/whoami':
+        if update['command'][0] == '/whoami':
             whoami(update)
-        elif update.command[0] == '/echo':
+        elif update['command'][0] == '/echo':
             echo_input(update)
-        elif update.command[0] == '/gethelp':
+        elif update['command'][0] == '/gethelp':
             perform_help(update)
         else:
             bad_request(update)
     else:
-        if hasattr(update.message, 'reply_to_message'):
+        if 'reply_to_message' in update['message']:
             perform_reply(update)
         else:
             perform_text(update)
 
+
 def whoami(update):
-    update.method = ['sendMessage']
-    update.post_fields[0].chat_id = update.message.chat.id
+    update['method'] = ['sendMessage']
+    update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
 
-    firstname = update.message.from_user.first_name
-    lastname = update.message.from_user.last_name
-    username = update.message.from_user.username
-    user_id = update.message.from_user.id
+    firstname = update['message']['from']['first_name']
+    lastname = update['message']['from']['last_name']
+    username = update['message']['from']['username']
+    user_id = update['message']['from']['id']
 
-    update.post_fields[0].text = f"Hello, {firstname} {lastname}!\nYour username is {username} and your user ID is {user_id}."
+    update['post_fields'][0]['text'] = f"Hello, {firstname} {lastname}!\nYour username is {username} and your user ID is {user_id}."
+
 
 def perform_help(update):
-    update.method = ['sendMessage']
-    update.post_fields[0].chat_id = update.message.chat.id
-    update.post_fields[0].text = 'What kind of help would you like? Type "tech", "billing", or "other".'
+    update['method'] = ['sendMessage']
+    update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
+    update['post_fields'][0]['text'] = 'What kind of help would you like? Type "tech", "billing", or "other".'
 
     keyboard = [
         [{'text': 'tech'}, {'text': 'billing'}],
         [{'text': 'other'}],
     ]
-    update.post_fields[0].reply_markup = json.dumps({
+    update['post_fields'][0]['reply_markup'] = json.dumps({
         'keyboard': keyboard,
         'one_time_keyboard': True,
     })
 
+
 def bad_request(update):
-    update.method = ['sendMessage']
-    update.post_fields[0].chat_id = update.message.chat.id
-    update.post_fields[0].text = f'You wrote "{update.message.text}". I don\'t understand that.'
-    update.post_fields[0].disable_web_page_preview = True
+    update['method'] = ['sendMessage']
+    update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
+    update['post_fields'][0]['text'] = f'You wrote "{update["message"]["text"]}". I don\'t understand that.'
+    update['post_fields'][0]['disable_web_page_preview'] = True
+
 
 def perform_reply(update):
-    update.method = ['sendMessage']
-    update.post_fields[0].chat_id = update.message.chat.id
-    reply_text = update.message.reply_to_message.text
+    update['method'] = ['sendMessage']
+    update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
+    reply_text = update['message']['reply_to_message']['text']
     if reply_text == 'Please describe your problem and I\'ll forward your message.':
         finish_reply(update)
 
-def finish_reply(update):
-    # Forward the message
-    update.post_fields[0].chat_id = 949587899
-    update.post_fields[0].text = (
-        "You received the following question:\n\n" +
-        update.message.text +
-        "\n\nIt was sent by\n\n" +
-        str(update.message.from_user)
-    )
-
-    # Confirm that the message has been forwarded
-    update.method.append('sendMessage')
-    update.post_fields.append({})
-    update.post_fields[1].chat_id = update.message.chat.id
-    update.post_fields[1].text = 'Your help request has been forwarded. You should get a response within 24 hours.'
 
 def perform_text(update):
-    update.method = ['sendMessage']
-    update.post_fields[0].chat_id = update.message.chat.id
+    update['method'] = ['sendMessage']
+    update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
 
-    text = update.message.text
+    text = update['message']['text']
     if text == 'tech':
-        update.post_fields[0].text = 'Tech info here.'
+        update['post_fields'][0]['text'] = 'Tech info here.'
     elif text == 'billing':
-        update.post_fields[0].text = 'Billing info here.'
+        update['post_fields'][0]['text'] = 'Billing info here.'
     elif text == 'other':
         forward_other(update)
     else:
-        update.post_fields[0].text = "I didn't understand that."
+        update['post_fields'][0]['text'] = "I didn't understand that."
+
+
+def finish_reply(update):
+    update['post_fields'][0]['chat_id'] = CUSTOMER_SERVICE_ID
+    update['post_fields'][0]['text'] = (
+        "You received the following question:\n\n" +
+        update['message']['text'] +
+        "\n\nIt was sent by\n\n" +
+        str(update['message']['from'])
+    )
+
+    update['method'].append('sendMessage')
+    update['post_fields'].append({})
+    update['post_fields'][1]['chat_id'] = update['message']['chat']['id']
+    update['post_fields'][1]['text'] = 'Your help request has been forwarded. You should get a response within 24 hours.'
+
 
 def forward_other(update):
-    update.method = ['sendMessage']
-    update.post_fields[0].chat_id = update.message.chat.id
-    update.post_fields[0].text = "Please describe your problem and I'll forward your message."
-    update.post_fields[0].reply_markup = json.dumps({
+    update['method'] = ['sendMessage']
+    update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
+    update['post_fields'][0]['text'] = "Please describe your problem and I'll forward your message."
+    update['post_fields'][0]['reply_markup'] = json.dumps({
         'force_reply': True,
     })
