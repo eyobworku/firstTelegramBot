7ebe3cd11b88f7ce7f7f3cb6644a486f58f132f8
eyob worku <eyobworku62@gmail.com>
2023-07-25 19:32:02 +0300

    telegram last pythoneverywhere

--- functions.py
+++ functions.py
@@ -1,23 +1,28 @@
 import json
 import requests
 import os
-import openpyxl
+from db import insert_users,select_user,upate_user,delete_user,readJsonXl,create_and_insert_table,insert_reults,select_results,select_keys
 
 BOT_TOKEN = '6003301685:AAFSmqvk4IWe7mn9xt2HsLagLMjV1CIzpJA'
 # methods = []
 # post_fields = [{}]
 
 
-def echo_input(update,chat_id=949587899):
+def echo_input(update):
         # Using the Telegram Bot API method "sendMessage",
         # https://core.telegram.org/bots/api#sendmessage
         # methods.append('sendMessage')
 
         # # It's going back to the same chat where it came from
         # post_fields[0]['chat_id'] = update['message']['chat']['id']
-        update['methods'].append('sendMessage')
-        update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
-        update['post_fields'][0]['text'] = json.dumps(update,indent=2)
+        if 'callback_query' in update and update['callback_query']:
+            update['methods'].append('sendMessage')
+            update['post_fields'][0]['chat_id'] = update['callback_query']['message']['chat']['id']
+            update['post_fields'][0]['text'] = json.dumps(update,indent=2)
+        else:
+            update['methods'].append('sendMessage')
+            update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
+            update['post_fields'][0]['text'] = json.dumps(update,indent=2)
 
         # The text being returned is just the whole object
         # post_fields[0]['disable_web_page_preview'] = True
@@ -31,33 +36,14 @@ def send_response(update):
             return response.json()
             # Process the response if needed
 
-def save_file(update):
-    myFile = {}
-    myFile['methods'] = []
-    myFile['post_fields'] = [{}]
-    myFile['methods'].append('getFile')
-    myFile['post_fields'][0]['file_id'] = update['message']['document']['file_id']
-    myFile = send_response(myFile)
-
-    url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{myFile['result']['file_path']}"
-    response = requests.get(url)
-    folder_path = 'savedFile'
-    os.makedirs(folder_path, exist_ok=True)
-
-    file_name = myFile['result']['file_path'].split('/')
-    file_name = file_name[-1]
-    my_file = os.path.join(folder_path,file_name)
-    if response.status_code == 200:
-        with open(my_file, 'wb') as file:
-            file.write(response.content)
-    myList = readJsonXl(my_file)
-
-    update['methods'].append('sendMessage')
-    update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
-    update['post_fields'][0]['text'] = json.dumps(myList,indent=2)
-
-
 def route_requests(update):
+    if 'callback_query' in update:
+        if update['callback_query']['data']=='student' or update['callback_query']['data']=='instructor':
+            perform_callback(update)
+        else:
+            deprt_callback(update)
+        return
+
     if 'entities' in update['message'] and update['message']['entities'] and update['message']['entities'][0]['type'] == 'bot_command':
         update['parameters'] = []
         update['command'] = [update['message']['text'][0:update['message']['entities'][0]['length']]]
@@ -70,12 +56,20 @@ def route_requests(update):
             update['post_fields'][0]['text'] ='who am i'
         elif update['command'][0] == '/echo':
             echo_input(update)
-        elif update['command'][0] == '/gethelp':
+        elif update['command'][0] == '/start':
+            view_menue(update)
+        elif update['command'][0] == '/register':
+            view_menue(update)
+        elif str(update['command'][0]).startswith("/file_"):
             update['methods'] = ['sendMessage']
             update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
-            update['post_fields'][0]['text'] ='get help'
-            # perform_help(update)
-
+            command = update['command'][0]
+            myTable = command.split('/')[-1]
+            result = select_keys(myTable,update['message']['chat']['id'])
+            if len(result) > 0:
+                update['post_fields'][0]['text'] = json.dumps(result,indent=2)
+            else:
+                update['post_fields'][0]['text'] = "Please login.\n/start"
         else:
             # bad_request(update)
             update['methods'].append('sendMessage')
@@ -85,110 +79,194 @@ def route_requests(update):
         save_file(update)
     else:
         if 'reply_to_message' in update['message']:
-            # perform_reply(update)
-            update['methods'].append('sendMessage')
-            update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
-            update['post_fields'][0]['text'] ='perform reply'
+            if update['message']['reply_to_message']['text']=='Enter your Id and Name.\n\ne.g 1372 Alazar':
+                save_data(update)
         else:
-            # # perform_text(update)
+            perform_text(update)
+
+def save_file(update):
+    chat_id = update['message']['chat']['id']
+    myUser = select_user(chat_id)
+    if 'chat_id' in myUser:
+        myFile = {}
+        myFile['methods'] = []
+        myFile['post_fields'] = [{}]
+        myFile['methods'].append('getFile')
+        myFile['post_fields'][0]['file_id'] = update['message']['document']['file_id']
+        myFile = send_response(myFile)
+
+        url = f"https://api.telegram.org/file/bot{BOT_TOKEN}/{myFile['result']['file_path']}"
+        response = requests.get(url)
+        folder_path = 'savedFile'
+        os.makedirs(folder_path, exist_ok=True)
+
+        file_name = myFile['result']['file_path'].split('/')
+        file_name = file_name[-1]
+        my_file = os.path.join(folder_path,file_name)
+        if response.status_code == 200:
+            with open(my_file, 'wb') as file:
+                file.write(response.content)
+        fileName, keys, result = create_and_insert_table(my_file)
+        if result:
+            new = insert_reults(chat_id,fileName,keys)
+            if new:
+                text = 'You uploaded succesfully'
+            else:
+                text = 'You did not uploaded succesfully insertion'
+        else:
+            text = 'You did not uploaded succesfully'
+
+        update['methods'].append('sendMessage')
+        update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
+        update['post_fields'][0]['text'] = text
+    else:
+        update['methods'].append('sendMessage')
+        update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
+        update['post_fields'][0]['text'] = 'Please login first.\n/start'
+
+def view_menue(update):
+    chat_id = update['message']['chat']['id']
+    myUser = select_user(chat_id)
+    if 'chat_id' in myUser:
+        update['methods'].append('sendMessage')
+        update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
+        if myUser['roll'] == 'student':
+            update['post_fields'][0]['text'] ='Welcome to our bot {}.'.format(myUser['name'])
+            keyboard = [
+            [{'text':'Show Posts'}],
+            [{'text':'Delete Account'}]
+        ]
+            update['post_fields'][0]['reply_markup'] = json.dumps({
+            'keyboard': keyboard,
+            'resize_keyboard':True,
+            'one_time_keyboard':True,
+            'is_persistent':True,
+            })
+        else:
+            update['post_fields'][0]['text'] ='Welcome to our bot Mr. {}.'.format(myUser['name'])
+            keyboard = [
+            [{'text':'Add new post'}],
+            [{'text':'Delete Account'}]
+        ]
+            update['post_fields'][0]['reply_markup'] = json.dumps({
+            'keyboard':keyboard,
+            'resize_keyboard':True,
+            'one_time_keyboard':True,
+            'is_persistent':True,
+            })
+    else:
+        update['methods'].append('sendMessage')
+        update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
+        update['post_fields'][0]['text'] ='Welcome to our bot.\nRegister as:'
+        keyboard = [
+        [{'text':'Student','callback_data':'student'}],
+        [{'text':'Instructor','callback_data':'instructor'}]
+    ]
+        update['post_fields'][0]['reply_markup'] = json.dumps({
+            'inline_keyboard':keyboard})
+
+def perform_callback(update):
+    if update['callback_query']['data']=='student':
+        roll = 'student'
+    else:
+        roll = 'instructor'
+
+    keyboard = [
+    [{'text':'SE','callback_data':f'{roll}_SE'},{'text':'IT','callback_data':f'{roll}_IT'}],
+    [{'text':'CS','callback_data':f'{roll}_CS'},{'text':'IS','callback_data':f'{roll}_IS'}]
+    ]
+    update['methods'].append('sendMessage')
+    update['post_fields'][0]['chat_id'] = update['callback_query']['message']['chat']['id']
+
+    update['post_fields'][0]['text'] = 'Choose your department!'#Enter your Id and Name.\n\ne.g 1372 Alazar
+    update['post_fields'][0]['reply_markup'] = json.dumps({
+        'inline_keyboard':keyboard,
+        'force_reply':True
+    })
+
+    update['post_fields'].append({})
+    update['methods'].append('answerCallbackQuery')
+    update['post_fields'][1]['callback_query_id']=update['callback_query']['id']
+
+def deprt_callback(update):
+    values = update['callback_query']['data']
+    value = values.split('_')
+    chat_id = update['callback_query']['message']['chat']['id']
+    result = insert_users(chat_id,value[0],value[1])
+    if result:
+        update['methods'].append('sendMessage')
+        update['post_fields'][0]['chat_id'] = update['callback_query']['message']['chat']['id']
+
+        update['post_fields'][0]['text'] = 'Enter your Id and Name.\n\ne.g 1372 Alazar'
+        update['post_fields'][0]['reply_markup'] = json.dumps({
+            'force_reply':True
+            })
+
+        update['post_fields'].append({})
+        update['methods'].append('answerCallbackQuery')
+        update['post_fields'][1]['callback_query_id']=update['callback_query']['id']
+
+def save_data(update):
+    chat_id = update['message']['chat']['id']
+    myUser = select_user(chat_id)
+    if 'chat_id' in myUser:
+        values = update['message']['text'].split()
+        result = upate_user(myUser['chat_id'],myUser['roll'],myUser['department'],values[0],values[1])
+        if result==True:
+            text = 'registered'
             update['methods'].append('sendMessage')
             update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
-            update['post_fields'][0]['text'] ='perform text'
-            # echo_input(update)
+            update['post_fields'][0]['text'] = text
+            if myUser['roll'] == 'student':
+                keyboard = [
+                [{'text':'Show Posts'}],
+                [{'text':'Delete Account'}]
+            ]
+                update['post_fields'][0]['reply_markup'] = json.dumps({
+                'keyboard': keyboard,
+                'resize_keyboard':True,
+                'one_time_keyboard':True,
+                'is_persistent':True,
+                })
+            else:
+                keyboard = [
+                [{'text':'Add new post'}],
+                [{'text':'Delete Account'}]
+            ]
+                update['post_fields'][0]['reply_markup'] = json.dumps({
+                'keyboard':keyboard,
+                'resize_keyboard':True,
+                'one_time_keyboard':True,
+                'is_persistent':True,
+                })
+        else:
+            text = 'not registered'
+            update['methods'].append('sendMessage')
+            update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
+            update['post_fields'][0]['text'] = text
+    else:
+        view_menue(update)
 
-def readJsonXl(file):
-    wb = openpyxl.load_workbook(file)
-    ws = wb.active
+def perform_text(update):
+    update['methods'].append('sendMessage')
+    update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
 
-    keys = [ws.cell(row=1,column=i).value for i in range(1,ws.max_column+1)]
-
-    values = list()
-    for value in ws.iter_rows(min_row=2,max_row=ws.max_row,min_col=1,max_col=ws.max_column,values_only=True):
-        values.append(value)
-    ##convert
-    myList = list()
-    for item in values:
-        myList.append({k: v for k,v in zip(keys,item)})
-    return myList
-
-
-
-# def whoami(update):
-#     update['method'] = ['sendMessage']
-#     update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
-
-#     firstname = update['message']['from']['first_name']
-#     lastname = update['message']['from']['last_name']
-#     username = update['message']['from']['username']
-#     user_id = update['message']['from']['id']
-
-#     update['post_fields'][0]['text'] = f"Hello, {firstname} {lastname}!\nYour username is {username} and your user ID is {user_id}."
-
-
-# def perform_help(update):
-#     update['method'] = ['sendMessage']
-#     update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
-#     update['post_fields'][0]['text'] = 'What kind of help would you like? Type "tech", "billing", or "other".'
-
-#     keyboard = [
-#         [{'text': 'tech'}, {'text': 'billing'}],
-#         [{'text': 'other'}],
-#     ]
-#     update['post_fields'][0]['reply_markup'] = json.dumps({
-#         'keyboard': keyboard,
-#         'one_time_keyboard': True,
-#     })
-
-
-# def bad_request(update):
-#     update['method'] = ['sendMessage']
-#     update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
-#     update['post_fields'][0]['text'] = f'You wrote "{update["message"]["text"]}". I don\'t understand that.'
-#     update['post_fields'][0]['disable_web_page_preview'] = True
-
-
-# def perform_reply(update):
-#     update['method'] = ['sendMessage']
-#     update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
-#     reply_text = update['message']['reply_to_message']['text']
-#     if reply_text == 'Please describe your problem and I\'ll forward your message.':
-#         finish_reply(update)
-
-
-# def perform_text(update):
-#     update['method'] = ['sendMessage']
-#     update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
-
-#     text = update['message']['text']
-#     if text == 'tech':
-#         update['post_fields'][0]['text'] = 'Tech info here.'
-#     elif text == 'billing':
-#         update['post_fields'][0]['text'] = 'Billing info here.'
-#     elif text == 'other':
-#         forward_other(update)
-#     else:
-#         update['post_fields'][0]['text'] = "I didn't understand that."
-
-
-# def finish_reply(update):
-#     update['post_fields'][0]['chat_id'] = CUSTOMER_SERVICE_ID
-#     update['post_fields'][0]['text'] = (
-#         "You received the following question:\n\n" +
-#         update['message']['text'] +
-#         "\n\nIt was sent by\n\n" +
-#         str(update['message']['from'])
-#     )
-
-#     update['method'].append('sendMessage')
-#     update['post_fields'].append({})
-#     update['post_fields'][1]['chat_id'] = update['message']['chat']['id']
-#     update['post_fields'][1]['text'] = 'Your help request has been forwarded. You should get a response within 24 hours.'
-
-
-# def forward_other(update):
-#     update['method'] = ['sendMessage']
-#     update['post_fields'][0]['chat_id'] = update['message']['chat']['id']
-#     update['post_fields'][0]['text'] = "Please describe your problem and I'll forward your message."
-#     update['post_fields'][0]['reply_markup'] = json.dumps({
-#         'force_reply': True,
-#     })
+    text = update['message']['text']
+    if text == 'Delete Account':
+        result = delete_user(update['message']['chat']['id'])
+        if result:
+            update['post_fields'][0]['text'] = 'Deleted.'
+        else:
+            update['post_fields'][0]['text'] = 'Not deleted.'
+    elif text == 'Add new post':
+        update['post_fields'][0]['text'] = 'Please send me an excel file.\nWhich should contain an Id column and\nother information for students'
+    elif text == 'Show Posts':
+        chat_id = update['message']['chat']['id']
+        result = select_results(chat_id)
+        if len(result) > 0:
+            text = 'Posts for you:\n\n{}'.format("\n".join(["/{}".format(item[0]) for item in result]))
+        else:
+            text = 'There is no items for you.'
+        update['post_fields'][0]['text'] = text
+    else:
+        update['post_fields'][0]['text'] = "I didn't understand that."
--- webhook.py
+++ webhook.py
@@ -1,47 +1,7 @@
 from flask import Flask, request
 from functions import *
-from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
-import os
-from dotenv import load_dotenv
-
-
-# # Define your bot token
-# app = Flask(__name__)
-# app.config['MONGO_URI']=os.getenv('MONGO_URI')
-# mongo = PyMongo()
-# mongo.init_app(app)
-
-# @app.route('/', methods=['POST'])
-# def webhook():
-#     # Get the JSON data from the request
-#     update = request.get_json()
-
-#     # Initialize two variables used to respond to Telegram
-#     methods = []
-#     post_fields = []
-
-#     # Do the thing
-#     route_requests(update)
-
-#     # Send it all to Telegram's servers using HTTP POST
-#     send_response(update)
-
-#     return 'OK'
-
-# @app.route('/', methods=['GET'])
-# def index():
-#     db = mongo.db
-#     # bool = db.user.insert_one({'name':'test'})
-#     # if(bool.acknowledged):
-#     return '<h1>Added user</h1>'
-#     # else:
-#     #     return '<h1>Not added user</h1>'
-
-# if __name__ == '__main__':
-#     app.run()
-
-
+# from bson.objectid import ObjectId
+# import os
 
 # Define your bot token
 app = Flask(__name__)
